ed86a8bb0525a441ed5dbded3c477d9f23ac180c
thomasbarrath <thomas.barrath@lanex.com>
2023-11-08 21:57:47 -0600

    Add project files.

---
+++ MauiAppMorseCode/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMorseCode
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppMorseCode/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿namespace MauiAppMorseCode
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        private string currentMorseCode = "";
+        private string decodedText = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Dot_Clicked(object sender, EventArgs e)
+        {
+            currentMorseCode += ".";
+            UpdateDotsAndDashesLabel();
+
+        }
+
+        private void Dash_Clicked(object sender, EventArgs e)
+        {
+            currentMorseCode += "-";
+            UpdateDotsAndDashesLabel();
+        }
+
+        private void Space_Clicked(object sender, EventArgs e)
+        {
+            if (currentMorseCode == "")
+            {
+                decodedText += " "; // Add a space for a new word
+            }
+            else
+            {
+                char letter = Morse.MorseCoder(currentMorseCode);
+                decodedText += letter;
+            }
+
+            currentMorseCode = ""; // Reset the current Morse code
+            UpdateDotsAndDashesLabel();
+            UpdateLettersAndWordsLabel();
+        }
+
+        private void UpdateDotsAndDashesLabel()
+        {
+            dotsAndDashesLabel.Text = currentMorseCode;
+            dotsAndDashesLabel2.Text = currentMorseCode;
+        }
+
+        private void UpdateLettersAndWordsLabel()
+        {
+            lettersAndWordsLabel.Text = decodedText;
+
+        }
+    }
+}
